95bf7cb04f46eab7773436fb3ea38104162db574
Joe Enos <jtenos1@gmail.com>
2020-12-31 09:48:47 -0700

    C# Line Reader.

---
+++ 2020-12-31 c# linereader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+var cities = new HashSet<string>();
+using var reader = new StreamReader("data.txt");
+string line;
+while ((line = reader.ReadLine()) != null) 
+{
+    string[] fields = line.Split('\t');
+    string city = fields[4];
+    cities.Add(city);
+}
+
+WriteLine(string.Join(", ", cities.OrderBy(x => x)));
