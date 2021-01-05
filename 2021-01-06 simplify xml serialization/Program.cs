e31e6fba5dcd4e26f94e97455fe6e6ad806d9abe
Joe Enos <jtenos1@gmail.com>
2021-01-05 10:32:45 -0700

    Simplify XML Serialization

---
+++ 2021-01-06 simplify xml serialization/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+var foo = new Foo { Name = "John Doe", Age = 34 };
+
+var settings = new XmlWriterSettings
+{
+    Indent = true,
+    OmitXmlDeclaration = true
+};
+var ns = new XmlSerializerNamespaces();
+ns.Add("", "");
+
+using var writer = XmlWriter.Create(Console.Out, settings);
+new XmlSerializer(typeof(Foo)).Serialize(writer, foo, ns);
+
