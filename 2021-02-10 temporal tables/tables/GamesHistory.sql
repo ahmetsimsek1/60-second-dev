7e17c1089fb927bda096493dabd0c658b2a9ef54
Joe Enos <jtenos1@gmail.com>
2021-02-10 22:26:31 -0700

    Temporal tables

---
+++ 2021-02-10 temporal tables/tables/GamesHistory.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dbo.GamesHistory (
+    GameID INT NOT NULL
+    ,Team1Score INT NOT NULL
+    ,Team2Score INT NOT NULL
+    ,GameDate DATETIME2 NOT NULL
+    ,AddDate DATETIME2 NOT NULL
+    ,_StartDateTime DATETIME2 NOT NULL
+    ,_EndDateTime DATETIME2 NOT NULL
+);
+GO
