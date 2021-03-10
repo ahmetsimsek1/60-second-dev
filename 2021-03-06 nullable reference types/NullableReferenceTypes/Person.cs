25c9e83a026c732806b86d4c7577c0be77e3d44f
Joe Enos <joe@noedgesoftware.com>
2021-03-09 20:03:41 -0700

    Adding nullable reference types.

---
+++ 2021-03-06 nullable reference types/NullableReferenceTypes/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Person
+{
+    public string FirstName { get; set; } = "";
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; } = "";
+
+    // This is dangerous:
+    public string FavoriteFood { get; set; } = default!;
+}
+
+// OR
+
+public class Person1
+{
+    public Person1(string firstName, string? middleName, string lastName)
+        => (FirstName, MiddleName, LastName) = (firstName, middleName, lastName);
+
+    public string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; }
+}
---
+++ 2021-03-06 nullable reference types/NullableReferenceTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+var person = new Person
+{
+    FirstName = "John",
+    LastName = "Adams"
+};
+
+Console.WriteLine(person.MiddleName);
+
+// Have to do a null-check or null conditional
+Console.WriteLine(person.MiddleName?.Length);
+
+// This line won't compile
+Console.WriteLine(person.MiddleName.Length);
+
+// This will compile, but could result in NullReferenceException:
+Console.WriteLine(person.MiddleName!.Length);
+
+// This one was set to null in the constructor, so we better
+// hope that someone set it to non-null before this runs:
+Console.WriteLine(person.FavoriteFood.Length);
+
+// Guaranteed to not be null (unless you specifically
+// set it to null! - which you generally should never do)
+Console.WriteLine(person.FirstName.Length);
