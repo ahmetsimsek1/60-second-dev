2cb5e83ca41a2bc662e09756cd00f598d53d7b60
Joe Enos <joe@checkmail.dev>
2021-05-14 16:46:44 -0700

    More local storage

---
+++ 2021-05-14 local storage/Pages/LocalStorageValues.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace _2021_05_14_local_storage.Pages
+{
+    public class LocalStorageValuesModel : PageModel
+    {
+      
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
